2936e7ed39bdf34464a1904e833330f06ae7fb6f
Christopher Moll <senormoll+github@gmail.com>
2013-08-19 12:48:49 -0400

    Updated namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ZFR\MailChimpBundle\DependencyInjection;
+namespace ZfrMailChimpBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
