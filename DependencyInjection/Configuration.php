565017df1133f62a697984475b2972ae42fb9a07
Christopher Moll <senormoll+github@gmail.com>
2013-08-19 12:46:25 -0400

    Changed zfr_mail_chimp to zfr_mailchimp

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('zfr_mail_chimp');
+        $rootNode = $treeBuilder->root('zfr_mailchimp');
 
         $rootNode
             ->children()
